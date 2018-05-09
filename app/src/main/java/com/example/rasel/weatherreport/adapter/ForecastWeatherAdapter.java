2c11c4a3693f28aa2805e27a3705a2e80f18d81b
Mobile App Develop <sparkitworld@gmail.com>
2018-05-09 14:28:40 +0600

    RecyclerView With Demo Data

--- app/src/main/java/com/example/rasel/weatherreport/adapter/ForecastWeatherAdapter.java
+++ app/src/main/java/com/example/rasel/weatherreport/adapter/ForecastWeatherAdapter.java
@@ -2,17 +2,20 @@ package com.example.rasel.weatherreport.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.rasel.weatherreport.R;
 import com.example.rasel.weatherreport.model.Demo;
-import com.example.rasel.weatherreport.model.ForecastWeatherResponse;
 
 import java.util.List;
 
 public class ForecastWeatherAdapter extends RecyclerView.Adapter<ForecastWeatherAdapter.ForecastViewHolder> {
 
-    List<Demo> forecastList;
+    private List<Demo> forecastList;
 
     public ForecastWeatherAdapter(List<Demo> forecastList) {
         this.forecastList = forecastList;
@@ -21,22 +24,38 @@ public class ForecastWeatherAdapter extends RecyclerView.Adapter<ForecastWeather
     @NonNull
     @Override
     public ForecastViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.forecast_view,parent,false);
+
+        return new ForecastViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ForecastViewHolder holder, int position) {
-
+        Demo demo = forecastList.get(position);
+        holder.textViewToday.setText(demo.getToday());
+        holder.textViewMaxValue.setText(demo.getMax());
+        holder.textViewMinValue.setText(demo.getMin());
+        holder.imageViewIcon.setImageResource(demo.getIcon());
+        holder.textViewDate.setText(demo.getDate());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return forecastList.size();
     }
 
-    public class ForecastViewHolder extends RecyclerView.ViewHolder{
-        public ForecastViewHolder(View itemView) {
+    class ForecastViewHolder extends RecyclerView.ViewHolder{
+        TextView textViewToday,textViewMinValue,textViewDate,textViewMaxValue;
+        ImageView imageViewIcon;
+        ForecastViewHolder(View itemView) {
             super(itemView);
+
+            textViewToday = itemView.findViewById(R.id.textViewToday);
+            textViewMinValue = itemView.findViewById(R.id.textViewMinValue);
+            textViewDate = itemView.findViewById(R.id.textViewDate);
+            textViewMaxValue = itemView.findViewById(R.id.textViewMaxValue);
+            imageViewIcon = itemView.findViewById(R.id.imageViewIcon);
         }
     }
 }
--- app/src/main/java/com/example/rasel/weatherreport/fragment/FragmentWeek.java
+++ app/src/main/java/com/example/rasel/weatherreport/fragment/FragmentWeek.java
@@ -3,13 +3,18 @@ package com.example.rasel.weatherreport.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.rasel.weatherreport.R;
+import com.example.rasel.weatherreport.adapter.ForecastWeatherAdapter;
 import com.example.rasel.weatherreport.model.Demo;
 import com.example.rasel.weatherreport.model.ForecastWeatherResponse;
+
+import java.util.ArrayList;
 
 
 /**
@@ -17,7 +22,7 @@ import com.example.rasel.weatherreport.model.ForecastWeatherResponse;
  */
 public class FragmentWeek extends Fragment {
 
-
+    private RecyclerView recyclerView;
     public FragmentWeek() {
         // Required empty public constructor
     }
@@ -27,11 +32,25 @@ public class FragmentWeek extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fragment_week, container, false);
-        view.findViewById(R.id.recyclerView);
+        recyclerView = view.findViewById(R.id.recyclerView);
 
+        ArrayList<Demo> demos = new ArrayList<>();
 
+        Demo demo = new Demo("Today","25°C","45°C","9 May,2018",R.drawable.cloud);
+        Demo demo1 = new Demo("Today","25°C","45°C","9 May,2018",R.drawable.cloud);
+        Demo demo2 = new Demo("Today","25°C","45°C","9 May,2018",R.drawable.cloud);
+        Demo demo3 = new Demo("Today","25°C","45°C","9 May,2018",R.drawable.cloud);
 
+        demos.add(demo);
+        demos.add(demo1);
+        demos.add(demo2);
+        demos.add(demo3);
+        ForecastWeatherAdapter adapter = new ForecastWeatherAdapter(demos);
 
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
 
         return view;
     }
